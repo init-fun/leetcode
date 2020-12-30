03fbcb257eca3c210a0a35a594654641ce532c48
Kapil Raj <31207575+kapil-raj@users.noreply.github.com>
2020-12-30 22:49:17 +0530

    Time: 7084 ms (5.02%), Space: 14.9 MB (6.36%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -5,13 +5,18 @@ class Solution(object):
         :rtype: int
         """
         current_max = 0
+        
         for i in range(len(prices)-1):
+            # if the stock we have is the maximum then all the stock,
+            # then we will be losing money in that case
             if prices[i] > max(prices[i+1 : ]):
                 continue
             else:
-                max_output = max(prices[i+1: ]) - prices[i] #5
+                # check what maximum profit we can get, if we sell the current stock
+                # from the maximum prices stocks, check for each of the stock 
+                max_output = max(prices[i+1: ]) - prices[i]
                 if max_output > current_max:
-                    current_max = max_output #5
+                    current_max = max_output
                     
         return current_max
                 
