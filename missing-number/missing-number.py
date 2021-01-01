bd9e84e618a00f04263a3faa2ad29b33ad5cdcee
Kapil Raj <31207575+kapil-raj@users.noreply.github.com>
2021-01-01 12:29:57 +0530

    Time: 2060 ms (9.49%), Space: 14.7 MB (43.97%) - LeetHub

---
+++ missing-number/missing-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums)+1):
+            if i in nums:
+                continue
+            return i 
+            
+        
