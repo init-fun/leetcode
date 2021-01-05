567853664decf2e50e486dc8f9e8e3b9aed91bcf
Kapil Raj <31207575+kapil-raj@users.noreply.github.com>
2021-01-05 23:21:16 +0530

    Time: 64 ms (71.71%), Space: 17.8 MB (20.23%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.py
+++ find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,10 +1,4 @@
-class Solution(object):
-    def findDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        for num in nums:
-            if nums.count(num) >= 2:
-                return num
-    
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        d = collections.Counter(nums)
+        return max(d, key = d.get)
